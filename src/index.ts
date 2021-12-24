a1dd2903810e810c886805a38bf28f93c76cc2b1
Cindy Van Dooren <cindy.vandooren@gmail.com>
2021-12-24 20:36:39 +0100

    Add Husky for pre-commit linting

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import "./app.scss"
+import './app.scss';
 
-window.addEventListener("load", () => {
-  const header = document.createElement("h1")
-  header.innerText = "Webpack❤️TS"
+window.addEventListener('load', () => {
+  const header = document.createElement('h1');
+  header.innerText = 'Webpack❤️TS';
 
-  const body = document.querySelector("body")
-  body.appendChild(header)
-})
+  const body = document.querySelector('body');
+  body.appendChild(header);
+
+  console.log('testing for prettier to fix this');
+});
